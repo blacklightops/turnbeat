90937db90314961ec91ee4b61b9116dc8caae919
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-06-15 21:38:43 -0500

    Redis Input Works

--- inputs/redis/redis.go
+++ inputs/redis/redis.go
@@ -8,6 +8,7 @@ import (
 	"github.com/garyburd/redigo/redis"
 	"fmt"
 	"time"
+	"strings"
 )
 
 type RedisInput struct {
@@ -42,16 +43,16 @@ func (l *RedisInput) Init(config inputs.MothershipConfig) error {
 	l.Port = config.Port
 
 	l.DB = config.DB
-	
+
 	if config.Key == "" {
 		return errors.New("No Input Key specified")
 	}
-	l.Key = config.Key
+	l.Key = strings.TrimSpace(config.Key)
 
 	if config.Type == "" {
 		return errors.New("No Event Type specified")
 	}
-	l.Type = config.Type
+	l.Type = strings.TrimSpace(config.Type)
 
 	logp.Debug("redisinput", "Using Host %s", l.Host)
 	logp.Debug("redisinput", "Using Port %d", l.Port)
@@ -67,37 +68,63 @@ func (l *RedisInput) GetConfig() inputs.MothershipConfig {
 }
 
 func (l *RedisInput) Run(output chan common.MapStr) error {
-	logp.Info("[RedisInput] Running Redis Input")
-	redisHostname := fmt.Sprintf("%s:%d", l.Host, l.Port)
-	server, err := redis.Dial("tcp", redisHostname)
-	if err != nil {
-		logp.Err("couldn't start listening: " + err.Error())
-		return nil
-	}
-	logp.Info("[RedisInput] Connected to Redis Server")
-
+	logp.Debug("redisinput", "Running Redis Input")
 	// dispatch the master listen thread
-	go func(server redis.Conn) {
+	var existsScript = redis.NewScript(1, `return redis.call('EXISTS', KEYS[1])`)
+
+	go func() {
+		redisURL := fmt.Sprintf("redis://%s:%d/%d", l.Host, l.Port, l.DB)
+		dialConnectTimeout := redis.DialConnectTimeout(3 * time.Second)
+		dialReadTimeout := redis.DialReadTimeout(10 * time.Second)
+		var backOffCount = 0
+		var backOffDuration time.Duration = 5 * time.Second
 		for {
-			exists, err := redis.Bool(server.Do("EXISTS", redis.Args{}.Add(l.Key)))
+			logp.Debug("redisinput", "Connecting to: %s", redisURL)
+			server, err := redis.DialURL(redisURL, dialConnectTimeout, dialReadTimeout)
 			if err != nil {
-				logp.Err("An error occured while executing EXISTS command")
+				logp.Err("couldn't start listening: " + err.Error())
 				return
 			}
-			if exists != true {
-				logp.Err("Key %s does not exist!", l.Key)
+			logp.Debug("redisinput", "Connected to Redis Server")
+			reply, err := existsScript.Do(server, l.Key)
+			if err != nil {
+				logp.Err("An error occured while executing EXISTS command: %s\n", err)
 				return
 			}
-			l.handleConn(server, output)
+			exists, err := redis.Int(reply, err)
+			if err != nil {
+				logp.Err("An error occured while converting reply to Int: %s\n", err)
+				return
+			}
+			if exists == 1 {
+				lineCount, err := l.handleConn(server, output)
+				if err == nil {
+					backOffCount = 0
+					backOffDuration = time.Duration(backOffCount) * time.Second
+					time.Sleep(backOffDuration)
+				} else {
+					backOffCount++
+					backOffDuration = time.Duration(backOffCount) * time.Second
+					time.Sleep(backOffDuration)
+				}
+				logp.Debug("redisinput", "Read %v events", lineCount)
+			} else {
+				logp.Info("[RedisInput] Key %s does not exist", l.Key)
+				backOffCount++
+				backOffDuration = time.Duration(backOffCount) * time.Second
+				time.Sleep(backOffDuration)
+			}
+			defer server.Close()
 		}
-	}(server)
+	}()
 	return nil
 }
 
-func (l *RedisInput) handleConn(server redis.Conn, output chan common.MapStr) {
+func (l *RedisInput) handleConn(server redis.Conn, output chan common.MapStr) (uint64, error) {
 	var offset int64 = 0
 	var line uint64 = 0
 	var bytesread uint64 = 0
+	var popScript = redis.NewScript(1, `return redis.call('LPOP', KEYS[1])`)
 
 	logp.Debug("redisinput", "Reading events from %s", l.Key)
 
@@ -107,25 +134,32 @@ func (l *RedisInput) handleConn(server redis.Conn, output chan common.MapStr) {
 	}
 
 	for {
-		reply, err := server.Do("LPOP", redis.Args{}.Add(l.Key))
-		text, err := redis.String(reply, err)
-		bytesread += uint64(len(text))
-
+		reply, err := popScript.Do(server, l.Key)
 		if err != nil {
-			logp.Info("Unexpected state reading from %s; error: %s\n", l.Key, err)
-			return
+			logp.Info("[RedisInput] Unexpected state reading from %s; error: %s\n", l.Key, err)
+			return line, err
 		}
+		if reply == nil {
+			logp.Debug("redisinputlines", "No values to read in LIST: %s", l.Key)
+			return line, nil
+		}
+		text, err := redis.String(reply, err)
+		if err != nil {
+			logp.Info("[RedisInput] Unexpected state converting reply to String; error: %s\n", err)
+			return line, err
+		}
+		bytesread += uint64(len(text))
 
 		logp.Debug("redisinputlines", "New Line: %s", &text)
 
 		line++
 
 		event := common.MapStr{}
-		event["source"] = l.Key
+		event["source"] = strings.TrimSpace(l.Key)
 		event["offset"] = offset
 		event["line"] = line
-		event["message"] = text
-		event["type"] = l.Type
+		event["message"] = &text
+		event["type"] = strings.TrimSpace(l.Type)
 
 		event.EnsureTimestampField(now)
 		event.EnsureCountField()
@@ -136,4 +170,5 @@ func (l *RedisInput) handleConn(server redis.Conn, output chan common.MapStr) {
 		output <- event // ship the new event downstream
 	}
 	logp.Debug("redisinput", "Finished reading from %s", l.Key)
+	return line, nil
 }
